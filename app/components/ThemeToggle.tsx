2db7939b772d7b4bd5291330c881a58840349c0c
Michelle Lo <michellelo@live.ca>
2024-08-19 22:46:56 -0400

    Add ThemeToggle component

---
+++ app/components/ThemeToggle.tsx
@@ -0,0 +1,20 @@
+import { IconButton, useColorMode } from "@chakra-ui/react";
+import { FaSun, FaMoon } from "react-icons/fa";
+
+const ThemeToggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <IconButton
+      aria-label="Toggle theme"
+      icon={colorMode === "light" ? <FaMoon /> : <FaSun />}
+      onClick={toggleColorMode}
+      position="fixed"
+      top="10px"
+      right="10px"
+      bg="transparent"
+      _hover={{ bg: "transparent" }}
+    />
+  );
+};
+
+export default ThemeToggle;
