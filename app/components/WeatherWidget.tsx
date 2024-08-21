694d13036c9e17fb3ee160e0aa18ad3a828f63bb
Michelle Lo <michellelo@live.ca>
2024-08-21 17:56:35 -0400

    fixed widget size

--- app/components/WeatherWidget.tsx
+++ app/components/WeatherWidget.tsx
@@ -1,7 +1,17 @@
 "use client";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Box, Text, Image, Flex, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  useColorModeValue,
+  IconButton,
+  Spinner,
+} from "@chakra-ui/react";
+import { Rnd } from "react-rnd";
+import { FaChevronUp, FaChevronDown } from "react-icons/fa";
 
 interface WeatherData {
   name: string;
@@ -28,6 +38,7 @@ const WeatherWidget = () => {
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [collapsed, setCollapsed] = useState(false);
 
   useEffect(() => {
     const fetchWeatherData = async () => {
@@ -47,13 +58,9 @@ const WeatherWidget = () => {
     fetchWeatherData();
   }, []);
 
-  if (loading) {
-    return <Text>Loading weather...</Text>;
-  }
-
-  if (error) {
-    return <Text>{error}</Text>;
-  }
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
 
   if (!weatherData) {
     return null;
@@ -64,62 +71,98 @@ const WeatherWidget = () => {
   const iconUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
 
   return (
-    <Box
-      p="4"
-      borderRadius="md"
-      bg={useColorModeValue("blue.300", "blue.800")}
-      color="white"
-      width="300px"
-      boxShadow="md"
-      textAlign="center"
+    <Rnd
+      default={{
+        x: 100,
+        y: 100,
+        width: 300,
+        height: collapsed ? 50 : 400,
+      }}
+      minHeight={50}
+      minWidth={150}
+      bounds="parent"
     >
-      <Text fontSize="xl" fontWeight="bold">
-        {weatherData.name}, {weatherData.sys.country}
-      </Text>
-      <Flex justify="center" align="center" mt="2">
-        <Image
-          src={iconUrl}
-          alt={weatherData.weather[0].description}
-          boxSize="80px"
+      <Box
+        p="4"
+        borderRadius="md"
+        bg={useColorModeValue("blue.300", "blue.800")}
+        color="white"
+        width="15rem"
+        boxShadow="md"
+        textAlign="center"
+      >
+        <IconButton
+          icon={collapsed ? <FaChevronDown /> : <FaChevronUp />}
+          size="xs"
+          onClick={toggleCollapse}
+          position="absolute"
+          top="2px"
+          left="13rem"
+          aria-label="Toggle collapse"
+          bg="transparent"
+          _hover={{ bg: "transparent" }}
+          color="white"
         />
-        <Text fontSize="4xl" ml="3">
-          {temperature}°C
-        </Text>
-      </Flex>
-      <Text fontSize="md" mt="1" fontStyle="italic">
-        {weatherData.weather[0].description}
-      </Text>
-      <Text mt="2">
-        Feels like:{" "}
-        <Text as="span" fontWeight="bold">
-          {feelsLike}°C
-        </Text>
-      </Text>
-      <Text mt="1">
-        Wind:{" "}
-        <Text as="span" fontWeight="bold">
-          {weatherData.wind.speed} m/s
-        </Text>
-      </Text>
-      <Text mt="1">
-        Humidity:{" "}
-        <Text as="span" fontWeight="bold">
-          {weatherData.main.humidity}%
-        </Text>
-      </Text>
-      <Text mt="1">
-        Pressure:{" "}
-        <Text as="span" fontWeight="bold">
-          {weatherData.main.pressure} hPa
-        </Text>
-      </Text>
-      <Text mt="1">
-        Visibility:{" "}
-        <Text as="span" fontWeight="bold">
-          {weatherData.visibility / 1000} km
-        </Text>
-      </Text>
-    </Box>
+        {loading ? (
+          <Flex justify="center" align="center" height="100%">
+            <Spinner size="xl" color="white" />
+          </Flex>
+        ) : error ? (
+          <Text>{error}</Text>
+        ) : collapsed ? (
+          <Text fontWeight="bold">Weather</Text>
+        ) : (
+          <>
+            <Text fontSize="xl" fontWeight="bold">
+              {weatherData.name}, {weatherData.sys.country}
+            </Text>
+            <Flex justify="center" align="center" mt="2">
+              <Image
+                src={iconUrl}
+                alt={weatherData.weather[0].description}
+                boxSize="80px"
+              />
+              <Text fontSize="4xl" ml="3">
+                {temperature}°C
+              </Text>
+            </Flex>
+            <Text fontSize="md" mt="1" fontStyle="italic">
+              {weatherData.weather[0].description}
+            </Text>
+            <Text mt="2">
+              Feels like:{" "}
+              <Text as="span" fontWeight="bold">
+                {feelsLike}°C
+              </Text>
+            </Text>
+            <Text mt="1">
+              Wind:{" "}
+              <Text as="span" fontWeight="bold">
+                {weatherData.wind.speed} m/s
+              </Text>
+            </Text>
+            <Text mt="1">
+              Humidity:{" "}
+              <Text as="span" fontWeight="bold">
+                {weatherData.main.humidity}%
+              </Text>
+            </Text>
+            <Text mt="1">
+              Pressure:{" "}
+              <Text as="span" fontWeight="bold">
+                {weatherData.main.pressure} hPa
+              </Text>
+            </Text>
+            <Text mt="1">
+              Visibility:{" "}
+              <Text as="span" fontWeight="bold">
+                {weatherData.visibility / 1000} km
+              </Text>
+            </Text>
+          </>
+        )}
+      </Box>
+    </Rnd>
   );
 };
 
