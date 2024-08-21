69571aa6b0c4ad2417770c4612b957ab66011d98
Michelle Lo <michellelo@live.ca>
2024-08-21 14:02:15 -0400

    add weather widget

---
+++ app/components/WeatherWidget.tsx
@@ -0,0 +1,20 @@
+"use client";
+import axios from "axios";
+import {
+  Box,
+  Text,
+  Image,
+  Flex,
+  useColorModeValue,
+} fro
+
+export interface WeatherData {
+}
+
+export default function WeatherWidget (props: WeatherData) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
