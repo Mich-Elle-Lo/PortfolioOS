f571ddc4ba9a43b6e0e3e07fdbe1f3176e86c374
Michelle Lo <michellelo@live.ca>
2024-08-21 23:33:45 -0400

    api didnt play songs, used embedded playlist

--- app/components/SpotifyWindow.tsx
+++ app/components/SpotifyWindow.tsx
@@ -1,13 +1,4 @@
-import { useEffect, useState } from "react";
-import { getSpotifyToken, getSpotifyData } from "../utils/utils";
-import {
-  Box,
-  Image,
-  Text,
-  Flex,
-  Button,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import MacWindow from "./MacWindow";
 
 interface SpotifyWindowProps {
@@ -25,36 +16,6 @@ const SpotifyWindow: React.FC<SpotifyWindowProps> = ({
   zIndex,
   onClick,
 }) => {
-  const [tracks, setTracks] = useState([]);
-  const [currentTrack, setCurrentTrack] = useState(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const token = await getSpotifyToken();
-      const data = await getSpotifyData(
-        token,
-        "playlists/1K6znuBPBNo6Kpaizo6pyh?si=486f1c959cee4e64"
-      );
-      setTracks(data.tracks.items);
-      setCurrentTrack(data.tracks.items[0]);
-    };
-
-    fetchData();
-  }, []);
-
-  const handlePlayPause = () => {
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleTrackSelect = (track) => {
-    setCurrentTrack(track);
-    setIsPlaying(true);
-  };
-
-  const bg = useColorModeValue("gray.900", "gray.800");
-  const textColor = useColorModeValue("white", "gray.200");
-
   return (
     <MacWindow
       title="Spotify Player"
@@ -64,32 +25,14 @@ const SpotifyWindow: React.FC<SpotifyWindowProps> = ({
       initialX={initialX}
       initialY={initialY}
     >
-      <Box bg={bg} color={textColor} p="4" height="100%" borderRadius="md">
-        {currentTrack && (
-          <Box textAlign="center" mb="4">
-            <Image
-              src={currentTrack.track.album.images[0].url}
-              alt={currentTrack.track.name}
-              boxSize="200px"
-              borderRadius="md"
-              mx="auto"
-            />
-            <Text fontSize="lg" mt="2">
-              {currentTrack.track.name}
-            </Text>
-            <Text fontSize="sm" color="gray.400">
-              {currentTrack.track.artists[0].name}
-            </Text>
-            <Button
-              onClick={handlePlayPause}
-              colorScheme={isPlaying ? "red" : "green"}
-              width="full"
-              mt="4"
-            >
-              {isPlaying ? "Pause" : "Play"}
-            </Button>
-          </Box>
-        )}
+      <Box height="100%" width="100%" overflow="hidden">
+        <iframe
+          src="https://open.spotify.com/embed/playlist/1K6znuBPBNo6Kpaizo6pyh?utm_source=generator&theme=0"
+          width="100%"
+          height="100%"
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        ></iframe>
       </Box>
     </MacWindow>
   );
