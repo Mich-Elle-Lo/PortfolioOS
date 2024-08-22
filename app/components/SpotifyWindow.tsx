ff2734d7ef13c323dbace4ccdd88b86075e5ed55
Michelle Lo <michellelo@live.ca>
2024-08-21 22:56:55 -0400

    create spotify window

--- app/components/SpotifyWindow.tsx
+++ app/components/SpotifyWindow.tsx
@@ -1,9 +1,30 @@
 import { useEffect, useState } from "react";
 import { getSpotifyToken, getSpotifyData } from "../utils/utils";
+import {
+  Box,
+  Image,
+  Text,
+  Flex,
+  Button,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import MacWindow from "./MacWindow";
 
-type Props = {};
+interface SpotifyWindowProps {
+  onClose: () => void;
+  initialX: number;
+  initialY: number;
+  zIndex: number;
+  onClick: () => void;
+}
 
-const SpotifyWindow = (props: Props) => {
+const SpotifyWindow: React.FC<SpotifyWindowProps> = ({
+  onClose,
+  initialX,
+  initialY,
+  zIndex,
+  onClick,
+}) => {
   const [tracks, setTracks] = useState([]);
   const [currentTrack, setCurrentTrack] = useState(null);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -21,8 +42,28 @@ const SpotifyWindow = (props: Props) => {
 
     fetchData();
   }, []);
-  console.log(tracks);
-  return <div>SpotifyWindow</div>;
+
+  const bg = useColorModeValue("gray.900", "gray.800");
+  const textColor = useColorModeValue("white", "gray.200");
+
+  return (
+    <MacWindow
+      title="Spotify Player"
+      onClose={onClose}
+      onClick={onClick}
+      zIndex={zIndex}
+      initialX={initialX}
+      initialY={initialY}
+    >
+      <Box
+        bg={bg}
+        color={textColor}
+        p="4"
+        height="100%"
+        borderRadius="md"
+      ></Box>
+    </MacWindow>
+  );
 };
 
 export default SpotifyWindow;
